bc02b8589ee69b0b412fc4b2032ec82994e3989b
YairMendoza95 <joseyair.95@gmail.com>
2018-02-05 16:20:35 -0600

    Subiendo practica completa

--- Drag/main.js
+++ Drag/main.js
@@ -1,6 +1,11 @@
 window.addEventListener("load", init);
-
+var container;
+var elementoArrastrandose;
 function init(){
+	container = document.querySelector('.container');
+	container.addEventListener("dragover", dragSobreContainer, false);
+	container.addEventListener("dragleave", dragSalioContainer, false);
+	container.addEventListener("drop", manejarDrop, false);
 	var circles = document.getElementsByClassName('circle');
 	for (i in circles){
 		var circle = circles[i];
@@ -21,8 +26,32 @@ function random(min, max){
 
 function dragIniciado(e){
 	this.style.backgroundColor = 'blue';
+	elementoArrastrandose = this;
+	var padre = document.createElement("p");
+	var clon = this.cloneNode(true);
+	padre.appendChild(clon);
+	e.dataTransfer.setData("text", padre.innerHTML);
 }
 
 function dragFinalizado(e){
-	
+	this.style.backgroundColor = 'red';	
+}
+
+function dragSobreContainer(e){
+	// Este evento sirve para cuando el navegador esta haciendo otra cosa lo dejo y pueda hacer el drag
+	e.preventDefault();
+	this.classList.add('over');
+	return false;
+}
+
+function dragSalioContainer(e){
+	this.classList.remove('over');
+}
+
+function manejarDrop (e) {
+	e.preventDefault();
+	var datos = e.dataTransfer.getData('text');
+	this.innerHTML += datos;
+	elementoArrastrandose.parentNode.removeChild(elementoArrastrandose);
+	this.classList.remove('over');
 }
