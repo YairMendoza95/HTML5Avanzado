8d893042552aaff19fd84deea373703d31b73510
YairMendoza95 <joseyair.95@gmail.com>
2018-02-07 13:27:25 -0600

    Finalizacion de la practica con el API de video

--- Video/main.js
+++ Video/main.js
@@ -1,1 +1,40 @@
 window.addEventListener('load', init);
+var video, volumen, progreso;
+function init () {
+	video = document.getElementById('video');
+	progreso = document.getElementById('progreso');
+	volumen = document.getElementById('volumen');
+	document.getElementById('reproducir').addEventListener('click', ponerPlay);
+	document.getElementById('pausa').addEventListener('click', pausar);
+	document.getElementById('stop').addEventListener('click', parar);
+	volumen.addEventListener('change', cambiarVolumen);
+	video.addEventListener('timeupdate', actualizarTiempo);
+	video.addEventListener('loadedmetadata', duracion);
+	//progreso.max = video.duration;
+}
+
+function ponerPlay(){
+	video.play();
+}
+
+function pausar () {
+	video.pause();
+}
+
+function parar () {
+	video.pause();
+	video.currentTime = 0;
+}
+
+function cambiarVolumen () {
+	video.volume = volumen.value;
+}
+
+function actualizarTiempo () {
+	document.querySelector('#tiempo').innerHTML = video.currentTime;
+	progreso.value = video.currentTime;
+}
+
+function duracion () {
+	progreso.max = video.duration;	
+}
