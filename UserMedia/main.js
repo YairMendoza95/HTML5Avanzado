526a64f1586ba880b70024299be81042f26e8061
YairMendoza95 <joseyair.95@gmail.com>
2018-02-08 00:20:52 -0600

    Terminando practica de camara y microfono

--- UserMedia/main.js
+++ UserMedia/main.js
@@ -1,4 +1,23 @@
 window.addEventListener('load', init);
 function init () {
-	
+	// Sirve para verificar cual de todos es soportado por el navegador
+	/* navigator.getUserMedia = {
+		navigator.getUserMedia ||
+		navigator.webkitGetUserMedia ||
+		navigator.mozGetUserMedia ||
+		navigator.msGetUserMedia
+	}*/
+
+	var video = document.getElementById('video');
+	if(navigator.getUserMedia){
+		navigator.getUserMedia({video: true, audio: true}, (stream) => {
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+		}, (e) => {
+			console.log(e);
+		});
+	}
+	else {
+		alert("Tu navegador no soprta User Media");
+	}
 }
