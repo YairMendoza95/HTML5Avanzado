8b7c883f6fc6553f32f1e8232714e4829bb081af
YairMendoza95 <joseyair.95@gmail.com>
2018-02-10 01:11:11 -0600

    Dibujando rectangulos

--- Canvas/main.js
+++ Canvas/main.js
@@ -1,4 +1,14 @@
 window.addEventListener('load', init);
 function init () {
-	var imagen = docuemnt.getElementById('imagen');
+	var canvas = document.getElementById('imagen');
+	var contexto = canvas.getContext('2d');
+	// Cuadrado - Es la unica figura que se puede hacer con un solo método
+	contexto.fillStyle = '#890';
+	contexto.fillRect(100, 100, 200, 200);
+
+	// Rectangulo con solo borde
+	contexto.fillStyle = '#123456';
+	contexto.strokeRect(10, 10, 50, 50);
+
+
 }
