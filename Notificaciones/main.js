57ca6f39a6ad5571ee6f32a2e97de88397c3cecf
YairMendoza95 <joseyair.95@gmail.com>
2018-02-12 12:05:09 -0600

    Subiendo practica de Notifications API

--- Notificaciones/main.js
+++ Notificaciones/main.js
@@ -1,5 +1,22 @@
+if(window.webkitNotifications) {
+	alert("Tu navegador no soporta esta funcionalidad");
+}
 window.addEventListener('load', init);
 
 function init () {
-	// body... 
+	document.querySelector('#activar').addEventListener('click', () => {
+		if(window.webkitNotifications.checkPermission() == 0) createNotification();
+		else window.webkitNotifications.requestPermission();
+	});
+}
+
+function createNotification(){
+	var notificacion = window.webkitNotifications.createNotification(""/* Imagen */, "Uso de Notification API", "¡Omedeto!");
+	notificacion.show();
+	notificacion.addEventListener('display', () => {
+		document.querySelector('#aviso').innerHTML = "Mostrando notificación";
+	});
+	notificacion.addEventListener('close', () => {
+		document.querySelector('#aviso').innerHTML = "Notificación cerrada";
+	});
 }
